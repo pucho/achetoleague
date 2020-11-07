fb17ee34129e87365e1c94c6b064a9ec90501c40
Francisco Mazzoni <fran.mazzoni@gmail.com>
2020-11-07 11:34:05 -0300

    added tailwind support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from "next/dist/next-server/lib/router/router";
 
+import "../css/tailwind.css";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
