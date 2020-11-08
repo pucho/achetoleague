30a1d0de4cfb865d421f342dcefc4a3174c7d1db
Francisco Mazzoni <fran.mazzoni@gmail.com>
2020-11-08 19:14:36 -0300

    added latest event

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,5 @@
 //usable uuids
-// d12f3e23-deae-4b7f-a8e5-f49a195a2e5b
+//
 // 9ac46398-f344-4f59-8222-4da6680f70c8
 // 1f2d520a-cf50-4831-9d09-fe5ef363db51
 // 516b17c3-513e-42f1-ae51-6439a6ec7e87
@@ -29,6 +29,8 @@ export const users: User[] = [
   { name: "manije", id: "6652adb4-304e-44a0-aede-a95070529569" },
   { name: "etarra", id: "f44f1372-0872-44e4-a0b5-a9f518a86fec" },
   { name: "liderguitar", id: "616c1a7d-069c-4f89-97b9-317eba3668e5" },
+  { name: "pinpon", id: "d12f3e23-deae-4b7f-a8e5-f49a195a2e5b" },
+  { name: "ketu", id: "9ac46398-f344-4f59-8222-4da6680f70c8" },
 ];
 
 const donington = {
@@ -59,7 +61,6 @@ const donington = {
   ],
 };
 
-//not run yet
 const brandshatch = {
   name: "Brands Hatch GP",
   race1: [
@@ -84,8 +85,38 @@ const brandshatch = {
   ],
 };
 
+const redbullring = {
+  name: "Redbull Ring GP",
+  race1: [
+    { name: "etarra", id: "f44f1372-0872-44e4-a0b5-a9f518a86fec" },
+    { name: "pelado", id: "ba9b5968-5cc0-42b4-a093-5766fff6c07c" },
+    { name: "mcsida", id: "f332ba3a-caa6-4b0c-af5e-728d30471eba" },
+    { name: "pucho", id: "a25ca502-536a-4a12-a38b-373d64e35e09" },
+    { name: "pinpon", id: "d12f3e23-deae-4b7f-a8e5-f49a195a2e5b" },
+    { name: "santicacu", id: "8a89fb3c-86da-4f5a-8d2e-9950c3bf8339" },
+    { name: "liderguitar", id: "616c1a7d-069c-4f89-97b9-317eba3668e5" },
+    { name: "ketu", id: "9ac46398-f344-4f59-8222-4da6680f70c8" },
+    { name: "manije", id: "6652adb4-304e-44a0-aede-a95070529569" },
+  ],
+  race2: [
+    { name: "pinpon", id: "d12f3e23-deae-4b7f-a8e5-f49a195a2e5b" },
+    { name: "etarra", id: "f44f1372-0872-44e4-a0b5-a9f518a86fec" },
+    { name: "pelado", id: "ba9b5968-5cc0-42b4-a093-5766fff6c07c" },
+    { name: "santicacu", id: "8a89fb3c-86da-4f5a-8d2e-9950c3bf8339" },
+    { name: "pucho", id: "a25ca502-536a-4a12-a38b-373d64e35e09" },
+    { name: "mcsida", id: "f332ba3a-caa6-4b0c-af5e-728d30471eba" },
+    { name: "manije", id: "6652adb4-304e-44a0-aede-a95070529569" },
+    { name: "ketu", id: "9ac46398-f344-4f59-8222-4da6680f70c8" },
+    { name: "liderguitar", id: "616c1a7d-069c-4f89-97b9-317eba3668e5" },
+  ],
+  replays: [
+    "https://drive.google.com/file/d/1guLrFNAw6SKc2Ugx_ZC18ogUl7YuHAuG/view?usp=sharing",
+    "https://drive.google.com/file/d/1guLrFNAw6SKc2Ugx_ZC18ogUl7YuHAuG/view?usp=sharing",
+  ],
+};
+
 //update after each round
-export const events = [donington, brandshatch];
+export const events = [donington, brandshatch, redbullring];
 
 type raceType = "race1" | "race2";
 type raceResults = User[];
